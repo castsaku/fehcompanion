aa8ac47d2dbc7ab79f7c7d486d492b3e06687d86
castsaku <andres.herrerasan@gmail.com>
2018-06-30 03:23:43 -0400

    clean

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-export { default } from '../stats.json';
-
-export { getEventHeroes } from './temporal/events';
-
-export { getAllHeroes, getHero, getReleasedHeroes } from './heroes';
-
-export { getAllSkills, getSkillObject, getSkillType } from './skills';
