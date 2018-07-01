28af8c5aed91930258d6a3aa140e8ba4f1f54575
etito <edgardoat@gmail.com>
2018-07-01 16:15:09 -0400

    change folder

--- apk/lib/main.dart
+++ apk/lib/main.dart
@@ -1,149 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:fheroes/presenters/firstPage.dart' as first;
+import './secondPage.dart' as second;
+void main() => runApp(new MaterialApp(
 
-void main() => runApp(FriendlyChatApp());
-const String _name = "Your Name";
-class FriendlyChatApp extends StatelessWidget {
+  theme: new ThemeData(fontFamily: "fheroes"),
+  home: MyTabs(),
+));
+
+class MyTabs extends StatefulWidget{
   @override
-  Widget build(BuildContext c) {
-    return MaterialApp(
-      title: "Friendly chat APp",
-      home: ChatScreen(),
-    );
+  MyTabsState createState()=> MyTabsState();
+
+
+
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin{
+
+
+  TabController controller;
+  @override
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller= TabController(vsync: this,length: 2);
   }
-}
 
-class ChatScreen extends StatefulWidget {
   @override
-  State createState() => ChatScreenState();
-}
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
-class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
-  final TextEditingController _textController = new TextEditingController();
-  final List<ChatMessage> _messages= <ChatMessage>[];
-  bool _isComposing =false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("WEA"),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_,int index)=>_messages[index],
-              itemCount: _messages.length,
-            ),
-          ),
-          Divider(height: 1.0,),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor),
-              child: _buildTextComposer(),
-            ),
-        ],
-      ),
-    );
-  }
-  Widget _buildTextComposer() {
-    return new IconTheme(
-      data:IconThemeData(color: Theme.of(context).accentColor),
-      child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: new Row(
-            children: <Widget>[
-               Flexible(
-                child: TextField(
-                  controller: _textController,
-                  onChanged: (String text){
-                    setState(() {
-                      _isComposing=text.length>0;
-                    });
-                  },
-                  onSubmitted: _handleSubmitted,
-                  decoration: InputDecoration.collapsed(
-                      hintText: "send message "),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal:4.0),
-                child: IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: _isComposing ? ()=>_handleSubmitted(_textController.text)
-                      :null,
-                ),
-              )
-            ],
-          )),
-    );
+   return Scaffold(
+
+     /*appBar: AppBar(
+       title: Text('Pages'),
+       backgroundColor: Colors.deepOrange,
+     ),*/
+
+     bottomNavigationBar: Material(
+       //color: Color(0xFF344955),
+
+       color: Color(0xF0000000),
+       child: TabBar(
+         controller: controller,
+         tabs: <Tab>[
+           Tab(icon: Icon(Icons.event_note),text :'News'),
+           Tab(icon: Icon(Icons.people),text: "Heroes"),
+
+         ],
+
+       ),
+     ),
+     body: TabBarView(
+       controller: controller,
+       children: <Widget>[
+
+         second.Second(),
+         first.First(),
+       ],
+     ) ,
+   );
   }
 
-
-  void _handleSubmitted(String text) {
-    _textController.clear();
-    setState(() {
-      _isComposing=false;
-    });
-    ChatMessage message= ChatMessage(
-      text:  text,
-      animationController: AnimationController(
-        duration: Duration(milliseconds: 700),
-        vsync: this,
-      ),
-    );
-    setState(() {
-      _messages.insert(0, message);
-    });
-    message.animationController.forward();
-  }
-
-  @override
-  void dispose() {                                                   //new
-    for (ChatMessage message in _messages)                           //new
-      message.animationController.dispose();                         //new
-    super.dispose();                                                 //new
-  }
-
-
-}
-
-class ChatMessage extends StatelessWidget{
-  ChatMessage({this.text,this.animationController})
-  final String text; // ignore: missing_function_body
-  final AnimationController animationController;
-  @override
-  Widget build(BuildContext context) {
-    return SizeTransition(
-      sizeFactor: CurvedAnimation(
-        parent: animationController,curve: Curves.easeOut),
-        axisAlignment: 0.0,
-        child:
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(right: 16.0),
-                  child: CircleAvatar(child: Text(_name[0]),),
-                ),
-                new Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(_name,style: Theme.of(context).textTheme.subhead,),
-                      Container(
-                        margin: const EdgeInsets.only(top: 5.0),
-                        child: Text(text),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-    );
-  }
 }
--- apk/test/widget_test.dart
+++ apk/test/widget_test.dart
@@ -7,12 +7,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:fheroes/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new FriendlyChatApp());
+    await tester.pumpWidget(new MyTabs());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
